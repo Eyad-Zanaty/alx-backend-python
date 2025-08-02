7d6e6805f93d809d50278f555ce5237324c4cbff
EyadZn <84345073+Eyad-Zanaty@users.noreply.github.com>
2025-08-02 13:47:43 +0300

    Update models.py

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -10,17 +10,17 @@ class Message(models.Model):
     def __str__(self):
         return f"{self.sender.username} to {self.receiver.username}: {self.content[:30]}"
 
-
-class UnreadNotificationManager(models.Manager):
+# MessageHistory.objects.create
+class MessageHistory(models.Manager):
     def for_user(self, user):
         return self.get_queryset().filter(user=user, is_read=False).only('id', 'message', 'created_at')
 
 
 class Notification(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    edited_by = models.ForeignKey(User, on_delete=models.CASCADE)
     message = models.ForeignKey(Message, on_delete=models.CASCADE)
     is_read = models.BooleanField(default=False)
-    created_at = models.DateTimeField(auto_now_add=True)
+    edited_at = models.DateTimeField(auto_now_add=True)
 
     # ✅ edited Custom Manager
     objects = models.Manager()  # default
@@ -28,4 +28,5 @@ class Notification(models.Model):
 
     def __str__(self):
         return f"Notification for {self.user.username} about message {self.message.id}"
+
 
